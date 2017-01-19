27b5ea14ef70f97f13e2328ae9cf53bc9e756ac8
Fredrik Bixo <bixo@kth.se>
2017-01-19 17:46:23 +0100

    Initial Commit

---
+++ Goalz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goalz
+//
+//  Created by Fredrik Bixo on 2017-01-19.
+//  Copyright © 2017 FredrikBixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
